47e47800e903a51ca8aede5c3967a2e8f55d70e6
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-03 13:16:26 +0100

    JS function for seconds move added

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,15 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!')
+
+const secondHand = document.querySelector('.clock__second--js')
+
+function myDate() {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const secondsDegree = (seconds/60)*360 + 180;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    console.log(seconds);
+}
+
+setInterval(myDate, 1000);
