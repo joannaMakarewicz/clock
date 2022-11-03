873c28a6246530ee9cc8df10b21cf4a41e20a976
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-03 13:37:52 +0100

    JS function for minutes and hours move added

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,25 @@ import '../scss/main.scss';
 
 console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!')
 
-const secondHand = document.querySelector('.clock__second--js')
+const secondHand = document.querySelector('.clock__second--js');
+const minuteHand = document.querySelector('.clock__minute--js');
+const hourHand = document.querySelector('.clock__hour--js');
 
 function myDate() {
     const date = new Date();
+
     const seconds = date.getSeconds();
-    const secondsDegree = (seconds/60)*360 + 180;
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
-    console.log(seconds);
+    const secondsMove = (seconds/60)*360 + 180;
+    secondHand.style.transform = `rotate(${secondsMove}deg)`;
+
+    const minutes = date.getMinutes();
+    const minutesMove = (minutes/60)*360 +180;
+    minuteHand.style.transform = `rotate(${minutesMove}deg)`;
+
+    const hours = date.getHours();
+    const hoursMove = (hours/12)*360 +180;
+    hourHand.style.transform = `rotate(${hoursMove}deg)`;
+
 }
 
 setInterval(myDate, 1000);
