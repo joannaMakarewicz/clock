d0ef28f5b238d2d8143be814ad9b39c2ad2e9deb
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-04 14:25:26 +0100

    Changes for easier JS function

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
-
 console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!')
 
 const secondHand = document.querySelector('.clock__second--js');
@@ -15,21 +9,21 @@ const hourHand = document.querySelector('.clock__hour--js');
 function myDate() {
     
     const date = new Date();
-    const seconds = date.getSeconds();
 
-    if(seconds<=60){
-    const secondsMove = (seconds/60)*360+180;
+    const seconds = date.getSeconds() / 60;
+    const secondsMove = seconds * 360 ;
     secondHand.style.transform = `rotate(${secondsMove}deg)`;
-    console.log(seconds);}
 
-    const minutes = date.getMinutes();
-    const minutesMove = (minutes/60)*360 +180;
+    const minutes = (seconds + date.getMinutes()) / 60;
+    const minutesMove = minutes * 360;
     minuteHand.style.transform = `rotate(${minutesMove}deg)`;
 
-    const hours = date.getHours();
-    const hoursMove = (hours/12)*360 +180;
+    const hours = (minutes + date.getHours()) / 12;
+    const hoursMove = hours * 360;
     hourHand.style.transform = `rotate(${hoursMove}deg)`;
-
+    
 }
 
 setInterval(myDate, 1000);
+
+myDate();
