c2a0c1ad99d93f5c6428747f3f762fb0843e206e
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-04 15:00:23 +0100

    Meta information adedded

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,25 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!')
+console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
 
-const secondHand = document.querySelector('.clock__second--js');
-const minuteHand = document.querySelector('.clock__minute--js');
-const hourHand = document.querySelector('.clock__hour--js');
+const secondHand = document.querySelector(".clock__second--js");
+const minuteHand = document.querySelector(".clock__minute--js");
+const hourHand = document.querySelector(".clock__hour--js");
 
 function myDate() {
-    
-    const date = new Date();
+  const date = new Date();
 
-    const seconds = date.getSeconds() / 60;
-    const secondsMove = seconds * 360 ;
-    if(seconds!=1){
-    secondHand.style.transform = `rotate(${secondsMove}deg)`;}
+  const seconds = date.getSeconds() / 60;
+  const secondsMove = seconds * 360;
+  secondHand.style.transform = `rotate(${secondsMove}deg)`;
 
-    const minutes = (seconds + date.getMinutes()) / 60;
-    const minutesMove = minutes * 360;
-    minuteHand.style.transform = `rotate(${minutesMove}deg)`;
+  const minutes = (seconds + date.getMinutes()) / 60;
+  const minutesMove = minutes * 360;
+  minuteHand.style.transform = `rotate(${minutesMove}deg)`;
 
-    const hours = (minutes + date.getHours()) / 12;
-    const hoursMove = hours * 360;
-    hourHand.style.transform = `rotate(${hoursMove}deg)`;
-    
+  const hours = (minutes + date.getHours()) / 12;
+  const hoursMove = hours * 360;
+  hourHand.style.transform = `rotate(${hoursMove}deg)`;
 }
 
 setInterval(myDate, 1000);
